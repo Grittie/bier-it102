633ff3a218ac56038fd461861e3a9bb7bf9da43f
@gritla <lars.grit@hva.nl>
2023-06-09 11:06:41 +0200

    lucas is niet lief

--- resources/views/score-registration.blade.php
+++ resources/views/score-registration.blade.php
@@ -7,31 +7,33 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
 
                 <form method="POST" action="{{ route('score-store') }}">
                     @csrf
 
-                    <div class="form-group">
-                        <label for="user_id">Select User:</label>
-                        <select class="form-control" id="user_id" name="user_id">
+                    <div class="mb-4">
+                        <label for="user_id" class="block text-gray-700 font-bold mb-2">Select User:</label>
+                        <select class="form-select w-full" id="user_id" name="user_id">
                             @foreach ($users as $user)
                                 <option value="{{ $user->id }}">{{ $user->name }}</option>
                             @endforeach
                         </select>
                     </div>
 
-                    <div class="form-group">
-                        <label for="pitchers">Pitchers:</label>
-                        <input type="number" class="form-control" id="pitchers" name="pitchers">
+                    <div class="mb-4">
+                        <label for="pitchers" class="block text-gray-700 font-bold mb-2">Pitchers:</label>
+                        <input type="number" class="form-input w-full" id="pitchers" name="pitchers">
                     </div>
 
-                    <div class="form-group">
-                        <label for="shame">Shame:</label>
-                        <input type="number" class="form-control" id="shame" name="shame">
+                    <div class="mb-4">
+                        <label for="shame" class="block text-gray-700 font-bold mb-2">Shame:</label>
+                        <input type="number" class="form-input w-full" id="shame" name="shame">
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        Button
+                    </button>
                 </form>
 
             </div>
