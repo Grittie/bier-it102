5683c2ab897d1efe534eb3fb53157779555f9697
@gritla <lars.grit@hva.nl>
2024-09-03 03:13:22 +0200

    made some small margin changes for the new temp logo

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center">
     <img src="{{ asset('images/logo.png') }}" alt="Logo" width="30" style="margin-right: 10px;">
-    <img src="{{ asset('images/logo-text.png') }}" alt="Logo" width="200" style="margin-top: 5px;">
+    <img src="{{ asset('images/logo-text.png') }}" alt="Logo" width="200" style="margin-top: 4px; margin-left: -8px;">
 </div>
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
 <div style="display: flex; flex-direction: column; align-items: center; width: 100%; text-align: center;">
-    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 120px; margin-bottom: 16px;">
+    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 120px; margin-bottom: 8px;">
     <img src="{{ asset('images/logo-text.png') }}" alt="Logo" style="width: 400px;">
 </div>
