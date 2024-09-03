98c517c072ebe1f977323dc6660468253305c7f6
@gritla <lars.grit@hva.nl>
2024-09-03 03:22:42 +0200

    name change

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
 <div style="display: flex; flex-direction: column; align-items: center; width: 100%; text-align: center;">
-    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 120px; margin-bottom: 8px;">
+    <img src="{{ asset('images/logo_V2.png') }}" alt="Logo" style="width: 120px; margin-bottom: 8px;">
     <img src="{{ asset('images/logo-text.png') }}" alt="Logo" style="width: 400px;">
 </div>
